266069cb55c1554a4a98a2b0dafee6ba37eeaa9c
zhao-lilei <zhaolilei77@gmail.com>
2019-07-15 14:55:00 +0800

    通用的引用文件

---
+++ lib/commom_import.dart
@@ -0,0 +1,25 @@
+//外部引用的包
+export 'package:flutter/material.dart';
+export 'dart:io';
+export 'package:flutter_swiper/flutter_swiper.dart';
+export 'package:shared_preferences/shared_preferences.dart';
+export 'package:fluttertoast/fluttertoast.dart';
+export 'package:intro_slider/intro_slider.dart';
+export 'package:flutter_slidable/flutter_slidable.dart';
+export 'package:common_utils/common_utils.dart';
+export 'package:event_bus/event_bus.dart';
+export 'package:share/share.dart';
+export 'package:photo_view/photo_view.dart';
+export 'package:flutter/services.dart';
+
+export 'app.dart';
+export 'splash_screen.dart';
+export 'loading_page.dart';
+
+//ui
+
+//home
+export 'package:flutter_study/ui/home/home_page.dart';
+
+//util
+export 'util/theme_util.dart';
