2ebd4ef079aa25f0523b37fd5270b939dd9a42df
zhao-lilei <zhaolilei77@gmail.com>
2019-07-16 10:21:34 +0800

    抽屉和搜索页面添加

---
+++ lib/ui/draw/draw_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_study/commom_import.dart';
+
+class DrawPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return DrawPageState();
+  }
+}
+
+class DrawPageState extends State<DrawPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
---
+++ lib/ui/search/search_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_study/commom_import.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SearchPageState();
+  }
+}
+
+class SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
